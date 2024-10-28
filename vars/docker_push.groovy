12f39753a15622879689a29cafdf311aa0f6836d
falgun-tsw <falgunthakore12@gmail.com>
2024-10-28 14:53:40 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,8 +3,8 @@ def call(String image_name, String tag) {
   withCredentials([usernamePassword(credentialsId: 'dockerHubCred', usernameVariable: 'dockerHubUser', passwordVariable: 'dockerHubPass')]) {
   echo "logging in docker"
   sh 'docker login -u $dockerHubUser -p $dockerHubPass'
-  echo "tagging image"
-  sh 'docker tag ${image_name} $dockerHubUser/${image_name}:${tag}'
+  // echo "tagging image"
+  // sh 'docker image tag ${image_name} $dockerHubUser/${image_name}:${tag}'
   echo "pushing to docker"
   sh 'docker push $dockerHubUser/${image_name}:${tag}'
   }
