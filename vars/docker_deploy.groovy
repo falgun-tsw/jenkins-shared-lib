82dca47cdedd20da9f913f911da978fd2ddc364c
falgun-tsw <falgunthakore12@gmail.com>
2024-10-28 15:22:46 +0530

    Create docker_deploy.groovy

---
+++ vars/docker_deploy.groovy
@@ -0,0 +1,5 @@
+def call(String image_name){
+  echo 'Deploying'
+  sh "docker rm $(docker stop $(docker ps -a -q  --filter ancestor=${image_name} --format="{{.ID}}"))"
+  sh "docker run -d -p 3000:3000 ${image_name}"
+}
